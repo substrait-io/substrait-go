7d226a05da3a9b4d9de60f5120a567d781a3e21f
Chandra Sanapala <chandra@sundeck.io>
2024-07-18 22:37:55 +0530

    Refactor tests and add some tests.

--- plan/relations_test.go
+++ plan/relations_test.go
@@ -1,119 +1,159 @@
 package plan
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/substrait-io/substrait-go/expr"
 	"testing"
 )
 
-func TestProjectRel_Copy(t *testing.T) {
-	p := &ProjectRel{
-		input: &VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 5}}}},
-		exprs: []expr.Expression{expr.NewPrimitiveLiteral(1.0, false), expr.NewPrimitiveLiteral(2.0, false)},
-	}
-
-	newInputs := []Rel{&VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 6}}}}}
-	got, err := p.Copy(newInputs...)
-	if !assert.NoError(t, err, fmt.Sprintf("Copy(%v)", newInputs)) {
-		return
-	}
-	want := &ProjectRel{
-		input: newInputs[0],
-		exprs: p.exprs,
-	}
-	assert.Equalf(t, want, got, "Copy(%v)", newInputs)
-
-	// Copy with the same inputs and rewriteFunc is a no-op.
-	sameInputs := p.GetInputs()
-	got1, err := p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return e, nil
-	}, sameInputs...)
-	assert.NoError(t, err)
-	assert.Nil(t, got1)
-
-	// Copy with different inputs and rewriteFunc is a no-op.
-	got1, err = p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return e, nil
-	}, newInputs...)
-	want1 := &ProjectRel{
-		input: newInputs[0],
-		exprs: p.exprs,
-	}
-	assert.NoError(t, err)
-	assert.Equal(t, want1, got1)
-
-	// Copy with different inputs and rewriteFunc changes all expressions.
-	got2, err := p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return expr.NewPrimitiveLiteral(9.0, false), nil
-	}, newInputs...)
-	want2 := &ProjectRel{
-		input: newInputs[0],
-		exprs: []expr.Expression{expr.NewPrimitiveLiteral(9.0, false), expr.NewPrimitiveLiteral(9.0, false)},
-	}
-	assert.NoError(t, err)
-	assert.Equal(t, want2, got2)
+func noOpRewrite(e expr.Expression) (expr.Expression, error) {
+	return e, nil
 }
 
-func TestJoinRel_Copy(t *testing.T) {
-	p := &JoinRel{
-		left:           &VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 1}}}},
-		right:          &VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 2}}}},
-		joinType:       JoinTypeInner,
-		expr:           expr.NewPrimitiveLiteral(true, false),
-		postJoinFilter: expr.NewPrimitiveLiteral(true, false),
+func createVirtualTableReadRel(value int64) *VirtualTableReadRel {
+	return &VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: value}}}}
+}
+
+func createPrimitiveLiteral(value float64) expr.Expression {
+	return expr.NewPrimitiveLiteral(value, false)
+}
+
+func TestRelations_Copy(t *testing.T) {
+	initialProjectRel := &ProjectRel{input: createVirtualTableReadRel(5), exprs: []expr.Expression{createPrimitiveLiteral(1.0), createPrimitiveLiteral(2.0)}}
+	initialJoinRel := &JoinRel{left: createVirtualTableReadRel(1), right: createVirtualTableReadRel(2), joinType: JoinTypeInner, expr: expr.NewPrimitiveLiteral(true, false), postJoinFilter: expr.NewPrimitiveLiteral(true, false)}
+	initialCrossRel := &CrossRel{left: createVirtualTableReadRel(1), right: createVirtualTableReadRel(2)}
+	initialFilterRel := &FilterRel{input: createVirtualTableReadRel(1), cond: expr.NewPrimitiveLiteral(true, false)}
+	localFileReadRel := &LocalFileReadRel{items: []FileOrFiles{{Path: "path"}},
+		baseReadRel: baseReadRel{
+			filter:           expr.NewPrimitiveLiteral(true, false),
+			bestEffortFilter: expr.NewPrimitiveLiteral(true, false),
+		},
+	}
+	initialSetRel := &SetRel{inputs: []Rel{createVirtualTableReadRel(1), createVirtualTableReadRel(2), createVirtualTableReadRel(3)}, op: SetOpUnionAll}
+	initialExtensionLeafRel := &ExtensionLeafRel{}
+	initialAggregateRel := &AggregateRel{input: createVirtualTableReadRel(1), groups: [][]expr.Expression{{createPrimitiveLiteral(1.0)}},
+		measures: []AggRelMeasure{{filter: expr.NewPrimitiveLiteral(true, false)}}}
+
+	type relationTestCase struct {
+		name        string
+		relation    Rel
+		newInputs   []Rel
+		rewriteFunc func(expr.Expression) (expr.Expression, error)
+		expectedRel Rel
+	}
+	testCases := []relationTestCase{
+		{
+			name:        "ProjectRel Copy with new inputs",
+			relation:    initialProjectRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6)},
+			expectedRel: &ProjectRel{input: createVirtualTableReadRel(6), exprs: []expr.Expression{createPrimitiveLiteral(1.0), createPrimitiveLiteral(2.0)}},
+		},
+		{
+			name:        "ProjectRel Copy with same inputs and noOpRewrite",
+			relation:    initialProjectRel,
+			newInputs:   []Rel{initialProjectRel.input},
+			rewriteFunc: noOpRewrite,
+			expectedRel: nil,
+		},
+		{
+			name:        "ProjectRel Copy with new inputs and noOpRewrite",
+			relation:    initialProjectRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6)},
+			rewriteFunc: noOpRewrite,
+			expectedRel: &ProjectRel{input: createVirtualTableReadRel(6), exprs: []expr.Expression{createPrimitiveLiteral(1.0), createPrimitiveLiteral(2.0)}},
+		},
+		{
+			name:        "ProjectRel Copy with new inputs and rewriteFunc",
+			relation:    initialProjectRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6)},
+			rewriteFunc: func(e expr.Expression) (expr.Expression, error) { return createPrimitiveLiteral(9.0), nil },
+			expectedRel: &ProjectRel{input: createVirtualTableReadRel(6), exprs: []expr.Expression{createPrimitiveLiteral(9.0), createPrimitiveLiteral(9.0)}},
+		},
+		{
+			name:        "JoinRel Copy with new inputs",
+			relation:    initialJoinRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7)},
+			expectedRel: &JoinRel{left: createVirtualTableReadRel(6), right: createVirtualTableReadRel(7), joinType: JoinTypeInner, expr: expr.NewPrimitiveLiteral(true, false), postJoinFilter: expr.NewPrimitiveLiteral(true, false)},
+		},
+		{
+			name:        "JoinRel Copy with same inputs and noOpRewrite",
+			relation:    initialJoinRel,
+			newInputs:   []Rel{initialJoinRel.left, initialJoinRel.right},
+			rewriteFunc: noOpRewrite,
+			expectedRel: nil,
+		},
+		{
+			name:        "CrossRel Copy with new inputs",
+			relation:    initialCrossRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7)},
+			expectedRel: &CrossRel{left: createVirtualTableReadRel(6), right: createVirtualTableReadRel(7)},
+		},
+		{
+			name:        "CrossRel Copy with same inputs and noOpRewrite",
+			relation:    initialCrossRel,
+			newInputs:   initialCrossRel.GetInputs(),
+			rewriteFunc: noOpRewrite,
+			expectedRel: nil,
+		},
+		{
+			name:        "CrossRel Copy with new inputs and noOpRewrite",
+			relation:    initialCrossRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7)},
+			rewriteFunc: noOpRewrite,
+			expectedRel: &CrossRel{left: createVirtualTableReadRel(6), right: createVirtualTableReadRel(7)},
+		},
+		{
+			name:        "CrossRel Copy with new inputs and rewriteFunc",
+			relation:    initialCrossRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7)},
+			rewriteFunc: func(e expr.Expression) (expr.Expression, error) { return createPrimitiveLiteral(9.0), nil },
+			expectedRel: &CrossRel{left: createVirtualTableReadRel(6), right: createVirtualTableReadRel(7)},
+		},
+		{
+			name:        "FilterRel Copy with new inputs",
+			relation:    initialFilterRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6)},
+			expectedRel: &FilterRel{input: createVirtualTableReadRel(6), cond: expr.NewPrimitiveLiteral(true, false)},
+		},
+		{
+			name:        "LocalFileReadRel Copy with new inputs",
+			relation:    localFileReadRel,
+			newInputs:   []Rel{},
+			expectedRel: localFileReadRel,
+		},
+		{
+			name:        "SetRel Copy with new inputs",
+			relation:    initialSetRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7), createVirtualTableReadRel(8)},
+			expectedRel: &SetRel{inputs: []Rel{createVirtualTableReadRel(6), createVirtualTableReadRel(7), createVirtualTableReadRel(8)}, op: SetOpUnionAll},
+		},
+		{
+			name:        "ExtensionLeafRel Copy with new inputs",
+			relation:    initialExtensionLeafRel,
+			newInputs:   []Rel{},
+			expectedRel: initialExtensionLeafRel,
+		},
+		{
+			name:        "AggregateRel Copy with new inputs",
+			relation:    initialAggregateRel,
+			newInputs:   []Rel{createVirtualTableReadRel(6)},
+			expectedRel: &AggregateRel{input: createVirtualTableReadRel(6), groups: initialAggregateRel.groups, measures: initialAggregateRel.measures},
+		},
 	}
 
-	newInputs := []Rel{
-		&VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 6}}}},
-		&VirtualTableReadRel{values: []expr.StructLiteralValue{[]expr.Literal{&expr.PrimitiveLiteral[int64]{Value: 7}}}},
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var got Rel
+			var err error
+			if tc.rewriteFunc == nil {
+				got, err = tc.relation.Copy(tc.newInputs...)
+			} else {
+				got, err = tc.relation.CopyWithExpressionRewrite(tc.rewriteFunc, tc.newInputs...)
+			}
+			if !assert.NoError(t, err) {
+				return
+			}
+			assert.Equal(t, tc.expectedRel, got)
+		})
 	}
-	got, err := p.Copy(newInputs...)
-	if !assert.NoError(t, err, fmt.Sprintf("Copy(%v)", newInputs)) {
-		return
-	}
-	want := &JoinRel{
-		left:           newInputs[0],
-		right:          newInputs[1],
-		joinType:       p.joinType,
-		expr:           p.expr,
-		postJoinFilter: p.postJoinFilter,
-	}
-	assert.Equalf(t, want, got, "Copy(%v)", newInputs)
-
-	// Copy with the same inputs and rewriteFunc is a no-op.
-	sameInputs := p.GetInputs()
-	got1, err := p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return e, nil
-	}, sameInputs...)
-	assert.NoError(t, err)
-	assert.Nil(t, got1)
-
-	// Copy with different inputs and rewriteFunc is a no-op.
-	got1, err = p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return e, nil
-	}, newInputs...)
-	want1 := &JoinRel{
-		left:           newInputs[0],
-		right:          newInputs[1],
-		joinType:       p.joinType,
-		expr:           p.expr,
-		postJoinFilter: p.postJoinFilter,
-	}
-	assert.NoError(t, err)
-	assert.Equal(t, want1, got1)
-
-	// Copy with different inputs and rewriteFunc changes the condition.
-	got2, err := p.CopyWithExpressionRewrite(func(e expr.Expression) (expr.Expression, error) {
-		return expr.NewPrimitiveLiteral(false, false), nil
-	}, newInputs...)
-	want2 := &JoinRel{
-		left:           newInputs[0],
-		right:          newInputs[1],
-		joinType:       p.joinType,
-		expr:           expr.NewPrimitiveLiteral(false, false),
-		postJoinFilter: expr.NewPrimitiveLiteral(false, false),
-	}
-	assert.NoError(t, err)
-	assert.Equal(t, want2, got2)
 }
