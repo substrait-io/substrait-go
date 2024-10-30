1234bcba4cc732a89c1f2ae9dd6ddd609170896e
Chandra Sanapala <chandra@sundeck.io>
2024-10-30 08:49:09 +0530

    Parser changes

---
+++ parser/types/parse_test.go
@@ -0,0 +1,108 @@
+package types
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/substrait-io/substrait-go/types"
+	"github.com/substrait-io/substrait-go/types/integer_parameters"
+)
+
+func TestParseSubstraitType(t *testing.T) {
+	tests := []struct {
+		input     string
+		expected  string
+		shortName string
+		want      types.Type
+	}{
+		{"boolean", "boolean", "bool", &types.BooleanType{Nullability: types.NullabilityRequired}},
+		{"i8", "i8", "i8", &types.Int8Type{Nullability: types.NullabilityRequired}},
+		{"i16", "i16", "i16", &types.Int16Type{Nullability: types.NullabilityRequired}},
+		{"i32", "i32", "i32", &types.Int32Type{Nullability: types.NullabilityRequired}},
+		{"i64", "i64", "i64", &types.Int64Type{Nullability: types.NullabilityRequired}},
+		{"fp32", "fp32", "fp32", &types.Float32Type{Nullability: types.NullabilityRequired}},
+		{"fp64", "fp64", "fp64", &types.Float64Type{Nullability: types.NullabilityRequired}},
+		{"string", "string", "str", &types.StringType{Nullability: types.NullabilityRequired}},
+		{"binary", "binary", "bin", &types.BinaryType{Nullability: types.NullabilityRequired}},
+		{"timestamp", "timestamp", "ts", &types.TimestampType{Nullability: types.NullabilityRequired}},
+		{"timestamp_tz", "timestamp_tz", "tstz", &types.TimestampTzType{Nullability: types.NullabilityRequired}},
+		{"date", "date", "date", &types.DateType{Nullability: types.NullabilityRequired}},
+		{"time", "time", "time", &types.TimeType{Nullability: types.NullabilityRequired}},
+		{"uuid", "uuid", "uuid", &types.UUIDType{Nullability: types.NullabilityRequired}},
+		{"interval_year", "interval_year", "iyear", &types.IntervalYearType{Nullability: types.NullabilityRequired}},
+		{"interval_day", "interval_day", "iday", &types.IntervalDayType{Nullability: types.NullabilityRequired}},
+		{"I8", "i8", "i8", &types.Int8Type{Nullability: types.NullabilityRequired}},
+		{"Boolean", "boolean", "bool", &types.BooleanType{Nullability: types.NullabilityRequired}},
+		{"i16?", "i16?", "i16", &types.Int16Type{Nullability: types.NullabilityNullable}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			got, err := ParseSubstraitType(tt.input)
+			assert.NoError(t, err)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseSubstraitType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseFuncDefArgType(t *testing.T) {
+	concreteInt5 := integer_parameters.ConcreteIntParam(5)
+	concreteInt10 := integer_parameters.ConcreteIntParam(10)
+	concreteInt38 := integer_parameters.ConcreteIntParam(38)
+	variableIntL1 := integer_parameters.VariableIntParam("L1")
+	variableIntP := integer_parameters.VariableIntParam("P")
+	variableIntS := integer_parameters.VariableIntParam("S")
+	tests := []struct {
+		input     string
+		expected  string
+		shortName string
+		want      types.FuncDefArgType
+	}{
+		{"I8", "i8", "i8", &types.Int8Type{Nullability: types.NullabilityRequired}},
+		{"Boolean", "boolean", "bool", &types.BooleanType{Nullability: types.NullabilityRequired}},
+		{"i8", "i8", "i8", &types.Int8Type{Nullability: types.NullabilityRequired}},
+		{"string", "string", "str", &types.StringType{Nullability: types.NullabilityRequired}},
+		{"i16?", "i16?", "i16", &types.Int16Type{Nullability: types.NullabilityNullable}},
+		{"boolean", "boolean", "bool", &types.BooleanType{Nullability: types.NullabilityRequired}},
+
+		{"fixedchar<5>", "char<5>", "fchar", &types.ParameterizedFixedCharType{IntegerOption: &concreteInt5, Nullability: types.NullabilityRequired}},
+		{"decimal<10,5>", "decimal<10,5>", "dec", &types.ParameterizedDecimalType{Precision: &concreteInt10, Scale: &concreteInt5, Nullability: types.NullabilityRequired}},
+		{"list<decimal<10,5>>", "list<decimal<10,5>>", "list", &types.ParameterizedListType{Type: &types.ParameterizedDecimalType{Precision: &concreteInt10, Scale: &concreteInt5, Nullability: types.NullabilityRequired}, Nullability: types.NullabilityRequired}},
+		{"list?<decimal?<10,5>>", "list?<decimal?<10,5>>", "list", &types.ParameterizedListType{Type: &types.ParameterizedDecimalType{Precision: &concreteInt10, Scale: &concreteInt5, Nullability: types.NullabilityNullable}, Nullability: types.NullabilityNullable}},
+		{"struct<i16?,i32>", "struct<i16?, i32>", "struct", &types.ParameterizedStructType{Types: []types.FuncDefArgType{&types.Int16Type{Nullability: types.NullabilityNullable}, &types.Int32Type{Nullability: types.NullabilityRequired}}, Nullability: types.NullabilityRequired}},
+		{"map<boolean?,struct?<i16?,i32?,i64?>>", "map<boolean?, struct?<i16?, i32?, i64?>>", "map", &types.ParameterizedMapType{Key: &types.BooleanType{Nullability: types.NullabilityNullable}, Value: &types.ParameterizedStructType{Types: []types.FuncDefArgType{&types.Int16Type{Nullability: types.NullabilityNullable}, &types.Int32Type{Nullability: types.NullabilityNullable}, &types.Int64Type{Nullability: types.NullabilityNullable}}, Nullability: types.NullabilityNullable}, Nullability: types.NullabilityRequired}},
+		{"map?<boolean?,struct?<i16?,i32?,i64?>>", "map?<boolean?, struct?<i16?, i32?, i64?>>", "map", &types.ParameterizedMapType{Key: &types.BooleanType{Nullability: types.NullabilityNullable}, Value: &types.ParameterizedStructType{Types: []types.FuncDefArgType{&types.Int16Type{Nullability: types.NullabilityNullable}, &types.Int32Type{Nullability: types.NullabilityNullable}, &types.Int64Type{Nullability: types.NullabilityNullable}}, Nullability: types.NullabilityNullable}, Nullability: types.NullabilityNullable}},
+		{"precision_timestamp<5>", "precision_timestamp<5>", "prets", &types.ParameterizedPrecisionTimestampType{IntegerOption: &concreteInt5, Nullability: types.NullabilityRequired}},
+		{"precision_timestamp_tz<5>", "precision_timestamp_tz<5>", "pretstz", &types.ParameterizedPrecisionTimestampTzType{IntegerOption: &concreteInt5, Nullability: types.NullabilityRequired}},
+
+		{"varchar<L1>", "varchar<L1>", "vchar", &types.ParameterizedVarCharType{IntegerOption: &variableIntL1, Nullability: types.NullabilityRequired}},
+		{"fixedchar<L1>", "char<L1>", "fchar", &types.ParameterizedFixedCharType{IntegerOption: &variableIntL1, Nullability: types.NullabilityRequired}},
+		{"fixedbinary<L1>", "fixedbinary<L1>", "fbin", &types.ParameterizedFixedBinaryType{IntegerOption: &variableIntL1, Nullability: types.NullabilityRequired}},
+		{"precision_timestamp<L1>", "precision_timestamp<L1>", "prets", &types.ParameterizedPrecisionTimestampType{IntegerOption: &variableIntL1, Nullability: types.NullabilityRequired}},
+		{"precision_timestamp_tz<L1>", "precision_timestamp_tz<L1>", "pretstz", &types.ParameterizedPrecisionTimestampTzType{IntegerOption: &variableIntL1, Nullability: types.NullabilityRequired}},
+		{"decimal<P,S>", "decimal<P,S>", "dec", &types.ParameterizedDecimalType{Precision: &variableIntP, Scale: &variableIntS, Nullability: types.NullabilityRequired}},
+		{"decimal<38,S>", "decimal<38,S>", "dec", &types.ParameterizedDecimalType{Precision: &concreteInt38, Scale: &variableIntS, Nullability: types.NullabilityRequired}},
+		{"any", "any", "any", types.AnyType{Name: "any", Nullability: types.NullabilityRequired}},
+		{"any1?", "any1?", "any", types.AnyType{Name: "any1", Nullability: types.NullabilityNullable}},
+		{"list<decimal<P,S>>", "list<decimal<P,S>>", "list", &types.ParameterizedListType{Type: &types.ParameterizedDecimalType{Precision: &variableIntP, Scale: &variableIntS, Nullability: types.NullabilityRequired}, Nullability: types.NullabilityRequired}},
+		{"struct<list?<decimal<P,S>>, i16>", "struct<list?<decimal<P,S>>, i16>", "struct", &types.ParameterizedStructType{Types: []types.FuncDefArgType{&types.ParameterizedListType{Type: &types.ParameterizedDecimalType{Precision: &variableIntP, Scale: &variableIntS, Nullability: types.NullabilityRequired}, Nullability: types.NullabilityNullable}, &types.Int16Type{Nullability: types.NullabilityRequired}}, Nullability: types.NullabilityRequired}},
+		{"map<decimal<P,S>, i16>", "map<decimal<P,S>, i16>", "map", &types.ParameterizedMapType{Key: &types.ParameterizedDecimalType{Precision: &variableIntP, Scale: &variableIntS, Nullability: types.NullabilityRequired}, Value: &types.Int16Type{Nullability: types.NullabilityRequired}, Nullability: types.NullabilityRequired}},
+		{"precision_timestamp_tz?<L1>", "precision_timestamp_tz?<L1>", "pretstz", &types.ParameterizedPrecisionTimestampTzType{IntegerOption: &variableIntL1, Nullability: types.NullabilityNullable}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			got, err := ParseFuncDefArgType(tt.input)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, got.String())
+			//if tt.shortName != "" {
+			//	assert.Equal(t, tt.shortName, got.ShortString())
+			//	assert.NoError(t, err)
+			//	assert.Equal(t, reflect.TypeOf(tt.want), reflect.TypeOf(typ))
+			//}
+			assert.True(t, reflect.DeepEqual(got, tt.want), "ParseSubstraitType() = %v, want %v", got, tt.want)
+		})
+	}
+}
---
+++ parser/types/visitor.go
@@ -0,0 +1,283 @@
+package types
+
+import (
+	"strconv"
+
+	"github.com/antlr4-go/antlr/v4"
+	"github.com/substrait-io/substrait-go/parser/types/baseparser"
+	"github.com/substrait-io/substrait-go/types"
+	"github.com/substrait-io/substrait-go/types/integer_parameters"
+)
+
+type MyVisitor struct {
+	baseparser.SubstraitTypeVisitor
+}
+
+var _ baseparser.SubstraitTypeVisitor = &MyVisitor{}
+
+func NewMyVisitor() *MyVisitor {
+	return &MyVisitor{}
+}
+
+func (v *MyVisitor) Visit(tree antlr.ParseTree) interface{} {
+	if tree == nil {
+		return nil
+	}
+	return tree.Accept(v)
+}
+
+func (v *MyVisitor) VisitStartRule(ctx *baseparser.StartRuleContext) interface{} {
+	return v.Visit(ctx.Expr())
+}
+
+func (v *MyVisitor) VisitTypeStatement(ctx *baseparser.TypeStatementContext) interface{} {
+	return v.Visit(ctx.TypeDef())
+}
+
+func (v *MyVisitor) VisitParenExpression(ctx *baseparser.ParenExpressionContext) interface{} {
+	return v.Visit(ctx.Expr())
+}
+
+func (v *MyVisitor) VisitMultilineDefinition(*baseparser.MultilineDefinitionContext) interface{} {
+	panic("implement MultilineDefinition")
+}
+
+func (v *MyVisitor) VisitTypeLiteral(ctx *baseparser.TypeLiteralContext) interface{} {
+	return v.Visit(ctx.TypeDef())
+}
+
+func (v *MyVisitor) VisitLiteralNumber(ctx *baseparser.LiteralNumberContext) interface{} {
+	num, err := strconv.ParseInt(ctx.Number().GetText(), 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
+
+func (v *MyVisitor) VisitFunctionCall(*baseparser.FunctionCallContext) interface{} {
+	panic("implement FunctionCall")
+}
+
+func (v *MyVisitor) VisitBinaryExpr(*baseparser.BinaryExprContext) interface{} {
+	panic("implement BinaryExpr")
+}
+
+func (v *MyVisitor) VisitIfExpr(*baseparser.IfExprContext) interface{} {
+	panic("implement IfExpr")
+}
+
+func (v *MyVisitor) VisitNotExpr(*baseparser.NotExprContext) interface{} {
+	panic("implement NotExpr")
+}
+
+func (v *MyVisitor) VisitTernary(*baseparser.TernaryContext) interface{} {
+	panic("implement Ternary expr")
+}
+
+func (v *MyVisitor) VisitTypeDef(ctx *baseparser.TypeDefContext) interface{} {
+	if ctx.ParameterizedType() != nil {
+		return v.Visit(ctx.ParameterizedType())
+	}
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	if ctx.ScalarType() != nil {
+		scalarTypeExpr := v.Visit(ctx.ScalarType())
+		scalarType := scalarTypeExpr.(types.Type)
+		return scalarType.WithNullability(nullability)
+	}
+
+	return types.AnyType{Name: ctx.AnyVar().GetText(), Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitBoolean(*baseparser.BooleanContext) interface{} {
+	return &types.BooleanType{}
+}
+
+func (v *MyVisitor) VisitI8(*baseparser.I8Context) interface{} {
+	return &types.Int8Type{}
+}
+
+func (v *MyVisitor) VisitI16(*baseparser.I16Context) interface{} {
+	return &types.Int16Type{}
+}
+
+func (v *MyVisitor) VisitI32(*baseparser.I32Context) interface{} {
+	return &types.Int32Type{}
+}
+
+func (v *MyVisitor) VisitI64(*baseparser.I64Context) interface{} {
+	return &types.Int64Type{}
+}
+
+func (v *MyVisitor) VisitFp32(*baseparser.Fp32Context) interface{} {
+	return &types.Float32Type{}
+}
+
+func (v *MyVisitor) VisitFp64(*baseparser.Fp64Context) interface{} {
+	return &types.Float64Type{}
+}
+
+func (v *MyVisitor) VisitString(*baseparser.StringContext) interface{} {
+	return &types.StringType{}
+}
+
+func (v *MyVisitor) VisitBinary(*baseparser.BinaryContext) interface{} {
+	return &types.BinaryType{}
+}
+
+func (v *MyVisitor) VisitTimestamp(*baseparser.TimestampContext) interface{} {
+	return &types.TimestampType{}
+}
+
+func (v *MyVisitor) VisitTimestampTz(*baseparser.TimestampTzContext) interface{} {
+	return &types.TimestampTzType{}
+}
+
+func (v *MyVisitor) VisitDate(*baseparser.DateContext) interface{} {
+	return &types.DateType{}
+}
+
+func (v *MyVisitor) VisitTime(*baseparser.TimeContext) interface{} {
+	return &types.TimeType{}
+}
+
+func (v *MyVisitor) VisitIntervalDay(*baseparser.IntervalDayContext) interface{} {
+	return &types.IntervalDayType{}
+}
+
+func (v *MyVisitor) VisitIntervalYear(*baseparser.IntervalYearContext) interface{} {
+	return &types.IntervalYearType{}
+}
+
+func (v *MyVisitor) VisitUuid(*baseparser.UuidContext) interface{} {
+	return &types.UUIDType{}
+}
+
+func (v *MyVisitor) VisitUserDefined(*baseparser.UserDefinedContext) interface{} {
+	// TODO handle user defined type name & parameters
+	return &types.UserDefinedType{}
+}
+
+func (v *MyVisitor) VisitFixedChar(ctx *baseparser.FixedCharContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	length := v.Visit(ctx.GetLength()).(integer_parameters.IntegerParameter)
+	return &types.ParameterizedFixedCharType{IntegerOption: length, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitVarChar(ctx *baseparser.VarCharContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	length := v.Visit(ctx.GetLength()).(integer_parameters.IntegerParameter)
+	return &types.ParameterizedVarCharType{IntegerOption: length, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitFixedBinary(ctx *baseparser.FixedBinaryContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	length := v.Visit(ctx.GetLength()).(integer_parameters.IntegerParameter)
+	return &types.ParameterizedFixedBinaryType{IntegerOption: length, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitDecimal(ctx *baseparser.DecimalContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	precision := v.Visit(ctx.GetPrecision()).(integer_parameters.IntegerParameter)
+	scale := v.Visit(ctx.GetScale()).(integer_parameters.IntegerParameter)
+
+	return &types.ParameterizedDecimalType{Precision: precision, Scale: scale, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitPrecisionTimestamp(ctx *baseparser.PrecisionTimestampContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	length := v.Visit(ctx.GetPrecision()).(integer_parameters.IntegerParameter)
+	return &types.ParameterizedPrecisionTimestampType{IntegerOption: length, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitPrecisionTimestampTZ(ctx *baseparser.PrecisionTimestampTZContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+
+	length := v.Visit(ctx.GetPrecision()).(integer_parameters.IntegerParameter)
+	return &types.ParameterizedPrecisionTimestampTzType{IntegerOption: length, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitStruct(ctx *baseparser.StructContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+	var fieldTypes []types.FuncDefArgType
+	for _, expr := range ctx.AllExpr() {
+		fieldTypes = append(fieldTypes, v.Visit(expr).(types.FuncDefArgType))
+	}
+	return &types.ParameterizedStructType{Types: fieldTypes, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitList(ctx *baseparser.ListContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+	elementType := v.Visit(ctx.Expr()).(types.FuncDefArgType)
+	return &types.ParameterizedListType{Type: elementType, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitNStruct(*baseparser.NStructContext) interface{} {
+	panic("implement me")
+}
+
+func (v *MyVisitor) VisitMap(ctx *baseparser.MapContext) interface{} {
+	nullability := types.NullabilityRequired
+	if ctx.GetIsnull() != nil {
+		nullability = types.NullabilityNullable
+	}
+	keyType, keyOk := v.Visit(ctx.GetKey()).(types.FuncDefArgType)
+	valueType, valueOk := v.Visit(ctx.GetValue()).(types.FuncDefArgType)
+	if !keyOk || !valueOk {
+		panic("Map key or value type is not a FuncDefArgType")
+	}
+	return &types.ParameterizedMapType{Key: keyType, Value: valueType, Nullability: nullability}
+}
+
+func (v *MyVisitor) VisitTypeParam(ctx *baseparser.TypeParamContext) interface{} {
+	return types.StringParameter(ctx.Identifier().GetText())
+}
+
+func (v *MyVisitor) VisitNumericLiteral(ctx *baseparser.NumericLiteralContext) interface{} {
+	num, err := strconv.ParseInt(ctx.Number().GetText(), 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	return integer_parameters.NewConcreteIntParam(int32(num))
+}
+
+func (v *MyVisitor) VisitNumericParameterName(ctx *baseparser.NumericParameterNameContext) interface{} {
+	return integer_parameters.NewVariableIntParam(ctx.GetText())
+}
+
+func (v *MyVisitor) VisitNumericExpression(ctx *baseparser.NumericExpressionContext) interface{} {
+	// TODO handle numeric expression
+	return ctx.GetText()
+}
