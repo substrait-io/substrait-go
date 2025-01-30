47499509214d0280dbf9d477528698c2cbae7278
David Sisson <EpsilonPrime@users.noreply.github.com>
2025-01-29 23:12:11 -0800

    feat: Add AggregateRel.ToBuilder()

--- plan/builders.go
+++ plan/builders.go
@@ -799,7 +799,7 @@ func extractGroup(expressionReferences []uint32, combinationIndex int) []uint32 
 	return group
 }
 
-// addRollup constructs the rollup grouping strategy from the provided grouping references.
+// AddRollup constructs the rollup grouping strategy from the provided grouping references.
 func (arb *AggregateRelBuilder) AddRollup(groupingReferences []uint32) {
 	for i := len(groupingReferences); i > 0; i-- {
 		rollupSet := groupingReferences[:i]
@@ -807,7 +807,7 @@ func (arb *AggregateRelBuilder) AddRollup(groupingReferences []uint32) {
 	}
 }
 
-// addGroupingSet adds a new grouping set based on the provided grouping references.
+// AddGroupingSet adds a new grouping set based on the provided grouping references.
 func (arb *AggregateRelBuilder) AddGroupingSet(groupingReferences []uint32) {
 	arb.groupingReferences = append(arb.groupingReferences, groupingReferences)
 }
