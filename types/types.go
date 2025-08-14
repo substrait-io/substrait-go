84e76a8ee261a8ae0cf447b24c3c171ad0b0abc1
Victor Barua <victor.barua@datadoghq.com>
2025-08-14 16:10:49 -0700

    refactor: remove potentially misplaced comment

--- types/types.go
+++ types/types.go
@@ -501,13 +501,6 @@ type (
 		WithParameters([]interface{}) (Type, error)
 	}
 
-	// Parameterized types are used in function signatures and can be resolved to concrete types
-	// during type derivation. For example, VARCHAR<L> becomes VarCharType with a specific length.
-	// In this example, VARCHAR<L> (ParameterizedVarCharType) would be a FuncDefArgType and
-	// the resolved VARCHAR<3> (VarCharType) with a value for L would be a Type.
-	//
-	// types are either both a FuncDefArgType and a Type, or they have a parameterized "version" (a Go type) and a concrete "version" (another Go type))
-
 	FixedType interface {
 		CompositeType
 		WithLength(int32) FixedType
@@ -768,8 +761,8 @@ func TypeToProto(t Type) *proto.Type {
 
 type primitiveTypeIFace interface {
 	bool | int8 | int16 | ~int32 | ~int64 |
-		float32 | float64 | ~string |
-		[]byte | IntervalYearToMonth | IntervalDayToSecond | UUID
+	float32 | float64 | ~string |
+	[]byte | IntervalYearToMonth | IntervalDayToSecond | UUID
 }
 
 var emptyFixedChar FixedChar
