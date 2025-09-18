69b4f2b997823bbfa1156a5f2fcffc3d64112c86
Ben Bellick <ben.bellick@datadoghq.com>
2025-09-18 13:59:54 -0400

    add one more test to ensure that plans w/ only uri vs urn are equivalent

--- plan/plan_test.go
+++ plan/plan_test.go
@@ -1,13 +1,18 @@
 package plan
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/substrait-io/substrait-go/v6/expr"
 	"github.com/substrait-io/substrait-go/v6/extensions"
 	proto "github.com/substrait-io/substrait-protobuf/go/substraitpb"
+	extensionspb "github.com/substrait-io/substrait-protobuf/go/substraitpb/extensions"
+	"google.golang.org/protobuf/encoding/protojson"
+	protobuf "google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/testing/protocmp"
 )
 
@@ -49,4 +54,91 @@ func TestRelFromProto(t *testing.T) {
 		})
 	}
 
+}
+
+const sampleYAML = `---
+urn: extension:test:sample
+types:
+  - name: point
+    structure:
+      latitude: i32
+      longitude: i32
+  - name: line
+    structure:
+      start: point
+      end: point
+scalar_functions:
+  -
+    name: "add"
+    description: "Add two values."
+    impls:
+      - args:
+          - name: x
+            value: i8
+          - name: y
+            value: i8
+        options:
+          overflow:
+            values: [ SILENT, SATURATE, ERROR ]
+        return: i8`
+
+func TestPlanRoundTripURIAndURNEquivalence(t *testing.T) {
+	c := &extensions.Collection{}
+	err := c.Load("some/uri", strings.NewReader(sampleYAML))
+	require.NoError(t, err)
+
+	// Create a simple plan with only URI extensions
+	uriOnlyPlan := &proto.Plan{
+		ExtensionUris: []*extensionspb.SimpleExtensionURI{
+			{ExtensionUriAnchor: 1, Uri: "some/uri"},
+		},
+		Extensions: []*extensionspb.SimpleExtensionDeclaration{
+			{
+				MappingType: &extensionspb.SimpleExtensionDeclaration_ExtensionFunction_{
+					ExtensionFunction: &extensionspb.SimpleExtensionDeclaration_ExtensionFunction{
+						ExtensionUriReference: 1,
+						FunctionAnchor:        1,
+						Name:                  "add:i8_i8",
+					},
+				},
+			},
+		},
+		Relations: []*proto.PlanRel{},
+	}
+
+	// Create equivalent plan with only URN extensions
+	urnOnlyPlan := &proto.Plan{
+		ExtensionUrns: []*extensionspb.SimpleExtensionURN{
+			{ExtensionUrnAnchor: 1, Urn: "extension:test:sample"},
+		},
+		Extensions: []*extensionspb.SimpleExtensionDeclaration{
+			{
+				MappingType: &extensionspb.SimpleExtensionDeclaration_ExtensionFunction_{
+					ExtensionFunction: &extensionspb.SimpleExtensionDeclaration_ExtensionFunction{
+						ExtensionUrnReference: 1,
+						FunctionAnchor:        1,
+						Name:                  "add:i8_i8",
+					},
+				},
+			},
+		},
+		Relations: []*proto.PlanRel{},
+	}
+
+	planFromURI, err := FromProto(uriOnlyPlan, c)
+	require.NoError(t, err)
+
+	planFromURN, err := FromProto(urnOnlyPlan, c)
+	require.NoError(t, err)
+
+	protoFromURI, err := planFromURI.ToProto()
+	require.NoError(t, err)
+
+	protoFromURN, err := planFromURN.ToProto()
+	require.NoError(t, err)
+
+	// Both plans should produce identical protobuf output after round-trip conversion
+	assert.True(t, protobuf.Equal(protoFromURN, protoFromURI),
+		"Plans should be identical after round-trip conversion.\nURI-only plan: %s\nURN-only plan: %s",
+		protojson.Format(protoFromURI), protojson.Format(protoFromURN))
 }
