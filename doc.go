b8721252c9fb7140028e3eb17fb9c7442998c932
Samantha Lim <samantha.lim@datadoghq.com>
2026-02-02 15:39:04 -0500

    Adding back doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Package substraitgo contains the experimental go bindings for substrait
+// (https://substrait.io).
+//
+// Current generated proto substrait version: v0.64.0
+package substraitgo
