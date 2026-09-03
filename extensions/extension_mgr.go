3451e211b119e1f3b60f0b6679a85de602cad734
bvolpato <brunocvcunha@gmail.com>
2026-09-03 19:30:13 +0000

    fix(extensions): preserve imported anchor assignments

--- extensions/extension_mgr.go
+++ extensions/extension_mgr.go
@@ -511,11 +511,8 @@ func (e *set) DecodeType(anchor uint32) (id TypeID, ok bool) {
 func (e *set) GetTypeAnchor(id TypeID) uint32 {
 	a, ok := e.types[id]
 	if !ok {
-		_, err := e.addOrGetURN(id.URN)
-		if err != nil {
-			panic(err)
-		}
-		a = uint32(len(e.types)) + 1
+		e.addOrGetURN(id.URN)
+		a = unusedAnchor(e.typesMap)
 		e.encodeType(a, id)
 	}
 	return a
@@ -524,11 +521,8 @@ func (e *set) GetTypeAnchor(id TypeID) uint32 {
 func (e *set) GetFuncAnchor(id FunctionID) uint32 {
 	a, ok := e.funcs[id]
 	if !ok {
-		_, err := e.addOrGetURN(id.URN)
-		if err != nil {
-			panic(err)
-		}
-		a = uint32(len(e.funcs)) + 1
+		e.addOrGetURN(id.URN)
+		a = unusedAnchor(e.funcMap)
 		e.encodeFunc(a, id)
 	}
 	return a
@@ -537,14 +531,8 @@ func (e *set) GetFuncAnchor(id FunctionID) uint32 {
 func (e *set) GetTypeVariationAnchor(id TypeVariationID) uint32 {
 	a, ok := e.typeVariations[id]
 	if !ok {
-		_, err := e.addOrGetURN(id.URN)
-		if err != nil {
-			panic(err)
-		}
-		// add 1 to the length to avoid an anchor of 0
-		// so that it's easier to tell when there is no
-		// type variation.
-		a = uint32(len(e.typeVariations)) + 1
+		e.addOrGetURN(id.URN)
+		a = unusedAnchor(e.typeVariationMap)
 		e.encodeTypeVariation(a, id)
 	}
 	return a
@@ -574,19 +562,28 @@ func (e *set) FindURN(urn string) (uint32, bool) {
 	return 0, false
 }
 
-func (e *set) addOrGetURN(urn string) (uint32, error) {
-	for k, v := range e.urns {
-		if v == urn {
-			return k, nil
+// unusedAnchor preserves the dense allocation sequence while avoiding collisions
+// with imported anchors. Zero is reserved for the system-preferred type variation.
+func unusedAnchor[T any](anchors map[uint32]T) uint32 {
+	for anchor := uint32(len(anchors)) + 1; ; anchor++ {
+		if anchor == 0 {
+			continue
+		}
+		if _, exists := anchors[anchor]; !exists {
+			return anchor
 		}
 	}
-	sz := uint32(len(e.urns)) + 1
-	if _, ok := e.urns[sz]; ok {
-		return 0, fmt.Errorf("%w: URN anchor %d already exists", substraitgo.ErrKeyExists, sz)
-	}
+}
 
-	e.urns[sz] = urn
-	return sz, nil
+func (e *set) addOrGetURN(urn string) uint32 {
+	for k, v := range e.urns {
+		if v == urn {
+			return k
+		}
+	}
+	anchor := unusedAnchor(e.urns)
+	e.urns[anchor] = urn
+	return anchor
 }
 
 type TopLevel interface {
