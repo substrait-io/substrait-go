05370f6f2ce80ece16ed55b34f5244b375be89b6
Aleksandr Efimov <horsodilo@gmail.com>
2026-09-16 22:00:24 +0300

    test(plan): reduce absent common regression to one case

--- plan/rel_common_test.go
+++ plan/rel_common_test.go
@@ -10,26 +10,14 @@ import (
 	proto "github.com/substrait-io/substrait-protobuf/go/substraitpb"
 )
 
-func TestSetRelWithoutCommon(t *testing.T) {
+func TestRelFromProtoWithoutCommon(t *testing.T) {
 	input := createJoinInput("input")
-	for _, tc := range []struct {
-		name   string
-		common *proto.RelCommon
-	}{
-		{"absent", nil},
-		{"empty", &proto.RelCommon{}},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			wire := &proto.Rel{RelType: &proto.Rel_Set{Set: &proto.SetRel{
-				Common: tc.common,
-				Inputs: []*proto.Rel{input.ToProto(), input.ToProto()},
-				Op:     proto.SetRel_SET_OP_UNION_ALL,
-			}}}
-			var rel Rel
-			var err error
-			require.NotPanics(t, func() { rel, err = RelFromProto(wire, joinTestRegistry()) })
-			require.NoError(t, err)
-			assert.Equal(t, input.RecordType(), rel.RecordType())
-		})
-	}
+	wire := &proto.Rel{RelType: &proto.Rel_Set{Set: &proto.SetRel{
+		Inputs: []*proto.Rel{input.ToProto(), input.ToProto()},
+		Op:     proto.SetRel_SET_OP_UNION_ALL,
+	}}}
+
+	rel, err := RelFromProto(wire, joinTestRegistry())
+	require.NoError(t, err)
+	assert.Equal(t, input.RecordType(), rel.RecordType())
 }
