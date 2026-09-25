5a4886128ded7196f4046ba57e0849e338d94b0b
Kadin Rabo <kadinjrabo@gmail.com>
2026-09-25 11:14:24 -0400

    refactor(expr)!: move MaskExpression encoding and decoding to wire

--- wire/field_reference.go
+++ wire/field_reference.go
@@ -69,6 +69,44 @@ func listElementRefToProto(r *expr.ListElementRef) *proto.Expression_ReferenceSe
 	}
 }
 
+// MaskExpressionToProto encodes a mask expression as its protobuf message.
+func MaskExpressionToProto(e *expr.MaskExpression) *proto.Expression_MaskExpression {
+	return &proto.Expression_MaskExpression{
+		Select:                 maskStructSelectToProto(e.Select()),
+		MaintainSingularStruct: e.MaintainSingularStruct(),
+	}
+}
+
+func maskStructSelectToProto(m expr.MaskStructSelect) *proto.Expression_MaskExpression_StructSelect {
+	items := make([]*proto.Expression_MaskExpression_StructItem, len(m))
+	for i := range m {
+		items[i] = maskStructItemToProto(&m[i])
+	}
+	return &proto.Expression_MaskExpression_StructSelect{StructItems: items}
+}
+
+func maskSelectToProto(s expr.MaskSelect) *proto.Expression_MaskExpression_Select {
+	switch s := s.(type) {
+	case expr.MaskStructSelect:
+		return &proto.Expression_MaskExpression_Select{
+			Type: &proto.Expression_MaskExpression_Select_Struct{Struct: maskStructSelectToProto(s)},
+		}
+	default:
+		panic(fmt.Sprintf("wire: unhandled mask selection %T", s))
+	}
+}
+
+func maskStructItemToProto(m *expr.MaskStructItem) *proto.Expression_MaskExpression_StructItem {
+	var child *proto.Expression_MaskExpression_Select
+	if c := m.Child(); c != nil {
+		child = maskSelectToProto(c)
+	}
+	return &proto.Expression_MaskExpression_StructItem{
+		Field: m.Field(),
+		Child: child,
+	}
+}
+
 // RefSegmentFromProto decodes a reference segment from its protobuf message.
 func RefSegmentFromProto(p *proto.Expression_ReferenceSegment) expr.ReferenceSegment {
 	if p == nil {
@@ -94,4 +132,33 @@ func RefSegmentFromProto(p *proto.Expression_ReferenceSegment) expr.ReferenceSeg
 	}
 
 	return nil
+}
+
+// MaskExpressionFromProto decodes a mask expression from its protobuf message.
+func MaskExpressionFromProto(p *proto.Expression_MaskExpression) *expr.MaskExpression {
+	sel := make(expr.MaskStructSelect, len(p.Select.StructItems))
+	for i, item := range p.Select.StructItems {
+		var child expr.MaskSelect
+		if item.Child != nil {
+			child = maskSelectFromProto(item.Child)
+		}
+		sel[i] = expr.NewMaskStructItem(item.Field, child)
+	}
+	return expr.NewMaskExpression(sel, p.MaintainSingularStruct)
+}
+
+func maskSelectFromProto(p *proto.Expression_MaskExpression_Select) expr.MaskSelect {
+	switch s := p.Type.(type) {
+	case *proto.Expression_MaskExpression_Select_Struct:
+		items := make(expr.MaskStructSelect, len(s.Struct.StructItems))
+		for i, item := range s.Struct.StructItems {
+			var child expr.MaskSelect
+			if item.Child != nil {
+				child = maskSelectFromProto(item.Child)
+			}
+			items[i] = expr.NewMaskStructItem(item.Field, child)
+		}
+		return items
+	}
+	panic("unimplemented mask select type")
 }
