b02af10998042ddf86cdce27e875b2c4396378b0
Kadin Rabo <kadinjrabo@gmail.com>
2026-09-25 11:14:24 -0400

    refactor(types)!: move List type encoding and decoding to wire

--- wire/types.go
+++ wire/types.go
@@ -125,6 +125,8 @@ func TypeToProto(t types.Type) *proto.Type {
 		return structTypeToProto(t)
 	case *types.FuncType:
 		return funcTypeToProto(t)
+	case *types.ListType:
+		return listTypeToProto(t)
 	}
 	panic("unimplemented type")
 }
@@ -161,6 +163,13 @@ func funcTypeToProto(f *types.FuncType) *proto.Type {
 			ReturnType:     TypeToProto(f.ReturnType),
 			Nullability:    proto.Type_Nullability(f.Nullability),
 		}}}
+}
+
+func listTypeToProto(t *types.ListType) *proto.Type {
+	return &proto.Type{Kind: &proto.Type_List_{
+		List: &proto.Type_List{Nullability: proto.Type_Nullability(t.Nullability),
+			Type:                   TypeToProto(t.Type),
+			TypeVariationReference: t.TypeVariationRef}}}
 }
 
 // TypeParamToProto encodes a user-defined-type parameter.
@@ -388,6 +397,12 @@ func TypeFromProto(t *proto.Type) types.Type {
 			Nullability:    types.Nullability(t.Func.Nullability),
 			ParameterTypes: params,
 			ReturnType:     TypeFromProto(t.Func.ReturnType),
+		}
+	case *proto.Type_List_:
+		return &types.ListType{
+			Nullability:      types.Nullability(t.List.Nullability),
+			TypeVariationRef: t.List.TypeVariationReference,
+			Type:             TypeFromProto(t.List.Type),
 		}
 	}
 	panic("unimplemented type from proto")
