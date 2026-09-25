896519923e7f6767a7f9a0e67fd0fae6e9299a86
Kadin Rabo <kadinjrabo@gmail.com>
2026-09-25 11:14:23 -0400

    refactor(types)!: move Version encoding and decoding to wire

--- wire/types.go
+++ wire/types.go
@@ -9,11 +9,36 @@ import (
 	proto "github.com/substrait-io/substrait-protobuf/go/substraitpb"
 )
 
+// VersionToProto encodes a version as its protobuf message.
+func VersionToProto(v types.Version) *proto.Version {
+	return &proto.Version{
+		MajorNumber: v.MajorNumber,
+		MinorNumber: v.MinorNumber,
+		PatchNumber: v.PatchNumber,
+		GitHash:     v.GitHash,
+		Producer:    v.Producer,
+	}
+}
+
 // TypeToProto constructs the protobuf message for the given type.
 func TypeToProto(t types.Type) *proto.Type {
 	switch t := t.(type) {
 	}
 	panic("unimplemented type")
+}
+
+// VersionFromProto decodes a version from its protobuf message.
+func VersionFromProto(v *proto.Version) types.Version {
+	if v == nil {
+		return types.Version{Producer: "UNSET"}
+	}
+	return types.Version{
+		MajorNumber: v.MajorNumber,
+		MinorNumber: v.MinorNumber,
+		PatchNumber: v.PatchNumber,
+		GitHash:     v.GitHash,
+		Producer:    v.Producer,
+	}
 }
 
 // TypeFromProto returns the appropriate Type object from a protobuf type message.
